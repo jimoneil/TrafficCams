acd16953333c431bbcd9aa8bef7fd36837b23677
Jim O'Neil <jim.oneil@outlook.com>
2013-06-25 23:13:26 -0400

    use RichText in lieu of WebView for flyouts

---
+++ TrafficCams/Flyouts/Privacy.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+//
+// LICENSE: http://aka.ms/LicenseTerms-SampleApps
+//
+
+namespace TrafficCams.Flyouts
+{
+    public sealed partial class PrivacyFlyout : UserControl
+    {
+        public PrivacyFlyout()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
